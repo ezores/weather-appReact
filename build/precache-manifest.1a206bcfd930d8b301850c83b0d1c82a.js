155046b2ced59ea26f9aed1fdf89fcdd2ec649f1
ezores <53787848+ezores@users.noreply.github.com>
2020-08-23 11:32:23 -0400

    deploy my react app to github pages

---
+++ build/precache-manifest.1a206bcfd930d8b301850c83b0d1c82a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "111459ef58e85002611e7356b2722a27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cef3e70be0f41fd3235",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0a1c5f46257b00ccba4b",
+    "url": "/static/js/2.c9069f4c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c9069f4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cef3e70be0f41fd3235",
+    "url": "/static/js/main.a347ae4d.chunk.js"
+  },
+  {
+    "revision": "f294869ae1697dfe421d",
+    "url": "/static/js/runtime-main.7ecb0647.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
