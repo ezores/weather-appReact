2f0df197b81c8c40fdab82846c3a18f1288f52c9
ezores <53787848+ezores@users.noreply.github.com>
2020-08-23 11:36:53 -0400

    Updates

---
+++ precache-manifest.00a8b8a554a038725cf95cc9b2915564.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4670836811845be99281a8fc9c7fdcf9",
+    "url": "/weather-appReact/index.html"
+  },
+  {
+    "revision": "8669c8561a18d6f83d13",
+    "url": "/weather-appReact/static/css/2.1b185f3e.chunk.css"
+  },
+  {
+    "revision": "8447aa4f4024d6126509",
+    "url": "/weather-appReact/static/css/main.7104bf89.chunk.css"
+  },
+  {
+    "revision": "8669c8561a18d6f83d13",
+    "url": "/weather-appReact/static/js/2.69c7e696.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-appReact/static/js/2.69c7e696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8447aa4f4024d6126509",
+    "url": "/weather-appReact/static/js/main.5c848252.chunk.js"
+  },
+  {
+    "revision": "4b244b0775ad3f68e61f",
+    "url": "/weather-appReact/static/js/runtime-main.8ec6127a.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/weather-appReact/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/weather-appReact/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/weather-appReact/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/weather-appReact/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/weather-appReact/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/weather-appReact/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
